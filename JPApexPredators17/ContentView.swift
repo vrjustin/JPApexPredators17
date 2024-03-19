2fdd0beff6ddeeb68ffc67efcea0280b7b2f1ddd
Justin Maronde <myemailisjustin@me.com>
2024-03-19 15:56:57 -0400

    add in our listview of predators; styling; computed props for image on ApexPredator

--- JPApexPredators17/ContentView.swift
+++ JPApexPredators17/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let predators = Predators()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(predators.apexPredators) { predator in
+            HStack {
+                //Image
+                Image(predator.image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .shadow(color: .white, radius: 1)
+                
+                VStack(alignment: .leading) {
+                    //name
+                    Text(predator.name)
+                        .fontWeight(.bold)
+
+                    //type
+                    Text(predator.type.rawValue.capitalized)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .padding(.horizontal, 13)
+                        .padding(.vertical, 5)
+                        .background(predator.type.background)
+                        .clipShape(.capsule)
+                    
+                }
+            }
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 
